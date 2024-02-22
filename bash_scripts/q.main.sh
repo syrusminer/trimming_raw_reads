429a10a3d107774d016d54458cf8b564b2cc8d19
Syrus Miner <u1207142@lonepeak1.wasatch.peaks>
2024-02-22 13:36:37 -0700

    git push

--- bash_scripts/q.main.sh
+++ bash_scripts/q.main.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-#SBATCH --account=utu-biol4310
+#SBATCH --account=utu
 #SBATCH --partition=lonepeak
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH -o slurm-%j.out-%N
 #SBATCH -e slurm-%j.err-%N
 
-wd=~/BIOL_4310/Exercises/Exercise_4/trimming_raw_reads
+wd=~/Biol4310/Assignments/trimming_raw_reads
 cd $wd
 bash bash_scripts/trim_raw_reads.sh -l raw_reads_SRA_list.txt -d $wd
